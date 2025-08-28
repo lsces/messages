dec8aa80a601e4b4679f49137c283f21a7a52abc
lsces <lester@lsces.co.uk>
2025-08-28 07:09:53 +0100

    Restructure layout in line with namespace addition

--- includes/bit_setup_inc.php
+++ includes/bit_setup_inc.php
@@ -1,16 +1,15 @@
 <?php
 global $gBitSystem, $gBitUser, $gBitSmarty;
 
-$registerHash = array(
+$registerHash = [
 	'package_name' => 'messages',
-	'package_path' => dirname( dirname( __FILE__ ) ).'/',
-);
+	'package_path' => dirname( dirname( __FILE__ ) ) . '/',
+];
 $gBitSystem->registerPackage( $registerHash );
 
 if( $gBitSystem->isPackageActive( 'messages' ) && $gBitUser->hasPermission( 'p_messages_send' ) ) {
-	require_once( MESSAGES_PKG_CLASS_PATH.'Messages.php' );
+	require_once MESSAGES_PKG_CLASS_PATH . 'Messages.php';
 	$messages = new Messages();
 	$unreadMsgs = $messages->unreadMessages( $gBitUser->mUserId );
-	$gBitSmarty->assignByRef( 'unreadMsgs', $unreadMsgs );
+	$gBitSmarty->assign( 'unreadMsgs', $unreadMsgs );
 }
-?>
