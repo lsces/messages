8a048d40504fa3b26095c0568dd246fd40afe148
Lester Caine <lester@lsces.co.uk>
2026-05-16 13:42:47 +0100

    Drop group management ? allow post to role members

--- broadcast.php
+++ broadcast.php
@@ -35,14 +35,6 @@ if( isset( $_REQUEST['send'] ) ) {
 	}
 }
 
-if( $gBitUser->isAdmin() ) {
-	$pListHash = [ 'sort_mode' => 'group_id_asc' ];
-	$groups = $gBitUser->getAllGroups( $pListHash );
-} else {
-	$gBitUser->loadGroups();
-	$groups = &$gBitUser->mGroups;
-}
-$gBitSmarty->assign( 'groups', $groups );
 $gBitSmarty->assign( 'feedback', $feedback );
 
 $gBitSystem->display( 'bitpackage:messages/broadcast.tpl', NULL, [ 'display_mode' => 'display' ]);
